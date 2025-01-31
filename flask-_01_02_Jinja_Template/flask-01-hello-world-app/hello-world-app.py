bccdbd0d64ea4432621e47f4c433ca978dda2c95
emrahme <120596201+emrahme@users.noreply.github.com>
2025-01-31 13:03:37 -0500

    Finish flask1 update

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,28 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello WOrld"
+
+@app.route("/bye")
+def bye():
+    return "bye"
+
+@app.route("/third")
+def third():
+    return "Hello From My Third Path"
+
+@app.route("/third/subthird")
+def subthird():
+    return "This is a double path"
+
+@app.route("/fourth/<string:id>")
+def fourth(id):
+    output = f"Your id is: {id}"
+    return output
+
+
 
 
 
@@ -8,5 +30,5 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5000)
     # app.run(host= '0.0.0.0', port=8081)
